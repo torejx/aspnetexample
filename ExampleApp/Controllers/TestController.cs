be7aa63f5fc8e2137672ffded38b63641f294d99
Salvatore Caputi <salvatore.caputi@INTERLOGICA.LAN>
2018-01-17 11:29:23 +0100

    add model

---
+++ ExampleApp/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class TestController : Controller
+    {
+
+        [HttpGet("hello/{name}/{surname}")]
+        public string Hello(string name, string surname)
+        {
+            var person = new Person();
+            person.Name = name;
+            person.Surname = surname;
+            return $"Hello {person.Name} {person.Surname}";
+        }
+    }
+}
---
+++ ExampleApp/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Person
+    {
+        private string _name;
+        private string _surname;
+
+        public string Name
+        {
+            get {
+                return _name;
+            }
+            set {
+                _name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get => _surname;
+            set => _surname = value;
+        }
+
+        public Person()
+        {
+
+        }
+    }
+}
