cc32658f3c125a9b120bb9c72864d4ad7e4b24c3
Salvatore Caputi <salvatore.caputi@INTERLOGICA.LAN>
2018-01-17 15:00:41 +0100

    filter validation

--- ExampleApp/Controllers/TestController.cs
+++ ExampleApp/Controllers/TestController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Example.Models;
+using Example.Attributes;
 
 namespace Example.Controllers
 {
     [Route("api/[controller]")]
     public class TestController : Controller
     {
+        [ValidateModel]
         [HttpPost("hello")]
         public IActionResult Hello([FromBody] Person person)
         {
